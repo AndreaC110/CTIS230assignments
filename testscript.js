329a55639bad85ad1ef2167fda338fc324b0cfde
AndreaC110 <acazaresvazquez@guilford.edu>
2024-03-25 20:30:50 -0400

    new

--- testscript.js
+++ testscript.js
@@ -5,3 +5,19 @@ myitem = document.getElementById("firsttest");
     function onClick() {  
         myitem.style.color = "light gray";  
     }
+
+    thebutton = document.getElementById("Next Page");  
+otheritem = document.getElementById("buttontest");
+thebutton.addEventListener("click", onButtonClick);
+function onButtonClick() {  
+    otheritem.style.color = "Purple";  
+}
+
+textentry = document.getElementById("myinput");  
+textentry.addEventListener("change", onChange);
+function onChange() {  
+    newtext = myinput.value;  
+    otheritem.innerHTML = newtext;  
+}
+<strong>Look at this bold text!</strong>
+
